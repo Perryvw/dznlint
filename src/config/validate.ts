3465e2713a5cbb1e1b86fda008fcd3d552a7049c
Perryvw <perry.wesel@gmail.com>
2022-01-31 20:33:41 +0100

    configuration validation

--- src/config/validate.ts
+++ src/config/validate.ts
@@ -1,6 +1,16 @@
+import { DEFAULT_DZNLINT_CONFIG } from "./default-config";
 import { DznLintUserConfiguration } from "./dznlint-configuration";
 
-export function validateConfiguration(config: DznLintUserConfiguration): { valid: false, issues: string[] } | { valid: true } {
-    void config;
-    return { valid: true };
+export function validateConfiguration(
+    config: DznLintUserConfiguration
+): { valid: false; issues: string[] } | { valid: true } {
+    const issues = [];
+
+    for (const key in config) {
+        if (!(key in DEFAULT_DZNLINT_CONFIG)) {
+            issues.push(`Unknown configuration key: '${key}'`);
+        }
+    }
+
+    return issues.length == 0 ? { valid: true } : { valid: false, issues };
 }
