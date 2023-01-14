87d4743fe9dbacf9c956f3223d9667fbdfd411e1
Perryvw <perry.wesel@gmail.com>
2023-01-14 15:27:54 +0100

    Templated parser

---
+++ src/grammar/preprocess-grammar.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs";
+
+const GRAMMAR_PATH = "src/grammar/dzn-grammar.peg";
+const GRAMMAR_OUT_PATH = "src/grammar/dzn-grammar.gen.peg";
+
+const templateDefinitionPattern = /template\s+([a-zA-Z0-9_-]+)<([a-zA-Z0-9_-]+)>\s+([^\n]*)/g;
+
+let grammar = fs.readFileSync(GRAMMAR_PATH).toString();
+
+const templates = findTemplates(grammar);
+grammar = removeTemplateDefinitions(grammar);
+
+grammar = substituteTemplates(grammar, templates);
+
+// Write preprocessed grammar
+fs.writeFileSync(GRAMMAR_OUT_PATH, grammar);
+
+interface Template {
+    name: string;
+    variableName: string;
+    template: string;
+}
+
+function findTemplates(content: string): Template[] {
+    const templates: Template[] = [];
+
+    let match: RegExpExecArray | null;
+    templateDefinitionPattern.lastIndex = 0; // Match from start
+    while ((match = templateDefinitionPattern.exec(content))) {
+        if (!match) break;
+
+        const [, templateName, variableName, template] = match;
+        templates.push({ name: templateName, variableName, template: template.trim() });
+    }
+
+    return templates;
+}
+
+function removeTemplateDefinitions(content: string): string {
+    return content.replace(templateDefinitionPattern, "");
+}
+
+function substituteTemplates(content: string, templates: Template[]): string
+{
+    for (const template of templates)
+    {
+        content = content.replace(new RegExp(`${template.name}<[^>]*>`, "gm"), match => {
+            const argumentMatch = /<([^>]*)/.exec(match);
+            if (!argumentMatch) throw "incorrect template usage";
+
+            const argument = argumentMatch[1].trim();
+            return template.template.replace("$" + template.variableName, argument);
+
+        });
+    }
+    return content;
+}
