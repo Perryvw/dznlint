c24b38b0b98cc8dec3eb22b787dea620c2829290
Perryvw <perryvw@users.noreply.github.com>
2025-02-12 21:00:37 +0100

    Disable legacy linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,11 +8,5 @@ export default tseslint.config(
         ignores: ["dist/*", "node_modules/*", "**/*.js", "src/grammar/parser.ts"],
     },
     eslint.configs.recommended,
-    tseslint.configs.recommended,
-    {
-        rules: {
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/no-non-null-assertion": "off",
-        },
-    }
+    tseslint.configs.recommended
 );
